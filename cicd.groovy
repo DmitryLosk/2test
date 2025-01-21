db5edd5d9c84f889dbc92e1daae6d3a5799c4bfb
DmitryLosk <dloskut@gmail.com>
2025-01-21 16:45:43 +0300

    new

--- cicd.groovy
+++ cicd.groovy
@@ -19,6 +19,17 @@ pipeline {
 				git branch: 'main', url: 'https://github.com/DmitryLosk/2test.git'
 			}
 		}
+		stage('Install Helm') {
+			steps {
+				sh '''
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+'''
+			}
+		}
 		stage('Build Docker Image') {
 			steps {
 				script {
