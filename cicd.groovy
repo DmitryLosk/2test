02ea3c34f2e9759ec1310b81b1d3b052271619ff
DmitryLosk <dloskut@gmail.com>
2025-01-21 13:14:35 +0300

    new

--- cicd.groovy
+++ cicd.groovy
@@ -3,7 +3,7 @@ pipeline {
 
 	environment {
 		DOCKER_CREDENTIALS_ID = 'docker-cred'
-		DOCKER_IMAGE = "158.160.46.211:5005/tutorial-app:${GIT_COMMIT}"
+		DOCKER_IMAGE = "158.160.46.211:5005/tutorial-app:001"
 		HELM_CHART_PATH = './helm-chart'
 		KUBE_CONFIG = '~/.kube/config' // Убедитесь, что путь правильный
 	}
