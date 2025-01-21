249c7a117549bb1c7f9648c82f3e8039bed33fee
DmitryLosk <dloskut@gmail.com>
2025-01-21 13:10:04 +0300

    new

---
+++ cicd.groovy
@@ -0,0 +1,40 @@
+pipeline {
+	agent any
+
+	environment {
+		DOCKER_CREDENTIALS_ID = 'docker-cred'
+		DOCKER_IMAGE = "158.160.46.211:5005/tutorial-app:${GIT_COMMIT}"
+		HELM_CHART_PATH = './helm-chart'
+		KUBE_CONFIG = '~/.kube/config' // Убедитесь, что путь правильный
+	}
+
+	stages {
+		stage('Clone Repository') {
+			steps {
+				git 'https://github.com/DmitryLosk/2test.git'
+			}
+		}
+		stage('Build Docker Image') {
+			steps {
+				script {
+					docker.withRegistry('158.160.46.211:5005', DOCKER_CREDENTIALS_ID) {
+						def customImage = docker.build(DOCKER_IMAGE)
+						customImage.push()
+					}
+				}
+			}
+		}
+
+		stage('Deploy with Helm') {
+			steps {
+				script {
+					sh "helm upgrade --install myapp ${HELM_CHART_PATH} --set image.tag=${GIT_COMMIT}"
+				}
+			}
+		}
+	}
+
+	triggers {
+		githubPush()
+	}
+}
