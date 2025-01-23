6e42f81003b101815cbb92f652b17d40d2a25afb
DmitryLosk <dloskut@gmail.com>
2025-01-23 16:44:05 +0300

    new

--- cicd.groovy
+++ cicd.groovy
@@ -19,8 +19,9 @@ pipeline {
 		stage('Checkout') {
 			steps {
 				script {
-					TAG_NAME = sh(returnStdout: true, script: "git tag --sort=-creatordate | head -n 1").trim()
+					latestTag = sh(returnStdout: true, script: "git tag --sort=-creatordate | head -n 1").trim()
 					echo latestTag
+					TAG_NAME = latestTag
 					checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: 'https://github.com/DmitryLosk/2test.git']], branches: [[name: "refs/tags/${latestTag}"]]], poll: false
 
 				}
